0fb91d9c9597d66fb8b095c64620f4357e10c26d
Bastien <bastien.boymond@epitech.eu>
2023-01-15 21:47:34 +0100

    fix

--- connection.c
+++ connection.c
@@ -7,18 +7,12 @@
 
 #include "navy.h"
 
-void handle_usr1(__attribute__((unused))int signal, \
-__attribute__((unused))siginfo_t *siginfo, \
-__attribute__((unused))void *context)
-{
-    player.connected = 1;
-}
-
 void sig_handler(int signal, siginfo_t *siginfo, \
 __attribute__((unused))void *context)
 {
-    player.pid_j2 = siginfo->si_pid;
-    kill(player.pid_j2, SIGUSR1);
+    // dans la structure siginfo on a le pid du process qui a envoyé le signal
+    // utiliser cette sturcture pour recuperer le pid du process qui a envoyé le signal
+    // puis renvoyer le signal au process qui a envoyé le signal pour lui dire qu'on a bien recu son pid
     signal++;
 }
 
@@ -28,11 +22,10 @@ pid_t get_j2_pid(void)
 
     act.sa_sigaction = &sig_handler;
     act.sa_flags = SA_SIGINFO;
-    sigemptyset(&act.sa_mask);
-    sigaction(SIGUSR1, &act, NULL);
+
+    // grace a la structure sigaction on peut modifier le comportement du signal SIGUSR1 pour utiliser notre sig_handler et la structure siginfo_t
+    // (man sigaction, man sigemptyset)
     while (player.pid_j2 == 0);
     printf("enemy connected\n\n");
-    act.sa_sigaction = &handle_usr1;
-    sigaction(SIGUSR1, &act, NULL);
     return (player.pid_j2);
 }
